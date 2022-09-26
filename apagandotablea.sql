06f3535e3062a9b43e52fa9a927e1dffefc1636b
Weverton Santos <wever-santos@hotmail.com>
2022-09-26 12:22:55 -0300

    Apagando tabela

---
+++ apagandotablea.sql
@@ -0,0 +1,8 @@
+USE [SUCOS_VENDAS]
+
+DROP TABLE [TABELA DE CLIENTES 2]
+
+DROP TABLE [TABELA DE VENDEDORES 2]
+
+/* Comando USE = Usar (garantir o banco de dados que estou manipulando)
+Comando GO = Executa */
