c659a838b4f58d3e3b7f6e0b45422a281a98a6f0
Weverton Santos <wever-santos@hotmail.com>
2022-09-28 12:06:06 -0300

    Consultas com Funções

---
+++ consultasstring-Funcao.sql
@@ -0,0 +1,51 @@
+
+
+SELECT LTRIM('      OLA')
+
+SELECT RTRIM('OLA      ')
+
+SELECT CONCAT('OLA ','TUDO BEM')
+
+SELECT 'OLA ' + 'TUDO BEM'
+
+SELECT LEFT('RUA AUGUSTA',3) 
+
+SELECT RIGHT('RUA AUGUSTA',7) 
+
+SELECT UPPER('rua augusta')   
+
+SELECT LOWER('RUA AUGUSTA')   
+
+SELECT REPLACE('R. AUGUSTA','R.','RUA') 
+
+SELECT SUBSTRING('RUA AUGUSTA', 1, 3) 
+
+SELECT SUBSTRING('RUA AUGUSTA', 2, 4) 
+
+SELECT LEN('RUA AUGUSTA')  
+
+SELECT * FROM [TABELA DE CLIENTES]
+
+SELECT CONCAT(NOME, ' (', CPF, ') ') FROM [TABELA DE CLIENTES]
+
+SELECT	*
+FROM	[TABELA DE CLIENTES]
+
+SELECT	CONCAT
+		(
+		NOME
+		,' ( ', 'Endereço: ', [ENDERECO 1], ' ) '
+		,' ( ', 'Bairro: ',BAIRRO, ' ) '
+		,' ( ', 'Cidade: ',CIDADE, ' ) '
+		,' ( ', 'Estado:', ESTADO, ' ) '
+		)
+FROM	[TABELA DE CLIENTES]
+
+
+
+
+
+
+
+
+
