2856ce09a207d7165f7199509da9f87c02e4b438
Weverton Santos <wever-santos@hotmail.com>
2022-10-01 16:05:55 -0300

    Incluindo múltiplos dados com outro bancos e no mesmo banco

---
+++ incluindo-Múltiplos-dados.sql
@@ -0,0 +1,20 @@
+ 
+ INSERT INTO [PRODUTOS]
+ (
+ [CÓDIGO], [DESCRITOR], [SABOR], [TAMANHO], [EMBALAGEM], [PREÇO LISTA]
+ )
+ VALUES
+ ('1040107', 'Light - 350 ml - Melância', 'Melância', '350 ml', 'Lata', 4.56)
+,('1040108', 'Light - 350 ml - Graviola', 'Graviola', '350 ml', 'Lata', 4.00)
+,('1040109', 'Light - 350 ml - Açaí', 'Açaí', '350 ml', 'Lata', 5.60)
+,('1040110', 'Light - 350 ml - Jaca', 'Jaca', '350 ml', 'Lata', 6.00)
+,('1040111', 'Light - 350 ml - Manga', 'Manga', '350 ml', 'Lata', 3.50)
+
+INSERT INTO [CLIENTES]
+(
+[CPF], [NOME], [ENDEREÇO], [BAIRRO], [CIDADE], [ESTADO], [CEP], [DATA NASCIMENTO], [IDADE], [SEXO], [LIMITE CRÉDITO], [VOLUME COMPRA], [PRIMEIRA COMPRA]
+)
+VALUES
+ ('1471156710', 'Érica Carvalho' ,'R. Iriquitia', 'Jardins', 'São Paulo', 'SP', '80012212', '1990-09-01', 27, 'F', 17000, 24500, 0)
+,('19290992743','Fernando Cavalcante', 'R. Dois de Fevereiro', 'Água Santa', 'Rio de Janeiro', 'RJ', '22000000', '2000-02-12', 18, 'M', 100000, 20000, 1)
+,('2600586709', 'César Teixeira','Rua Conde de Bonfim', 'Tijuca', 'Rio de Janeiro', 'RJ', '22020001', '2000-03-12', 18, 'M', 120000, 22000, 0)
